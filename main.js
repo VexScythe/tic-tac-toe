7b979625fcffdb69c17a3a7548a3c6ee81f878d1
VexScythe <davide.ludovici@yahoo.it>
2024-03-18 16:34:00 +0100

    feat: create Gameboard module

---
+++ main.js
@@ -0,0 +1,38 @@
+const Gameboard = (() => {
+    let board;
+  
+    const createBoard = () => {
+        board = [];
+        for (let i = 0; i < 3; i++){
+            let row = [];
+            for (let j = 0; j < 3; j++){
+                row.push("");
+            }
+            board.push(row);
+        }
+    };
+  
+    const getBoard = () => board;
+  
+    const placeMarker = (row, col, marker) => {
+      if (board[row][col] === "") {
+        board[row][col] = marker;
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    const resetBoard = () => {
+      createBoard();
+    };
+  
+    createBoard();
+  
+    return {
+      getBoard,
+      placeMarker,
+      resetBoard
+    };
+  })();
+  

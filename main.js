f95bff42e903d3db9b3557052374defdfd7cf111
VexScythe <davide.ludovici@yahoo.it>
2024-03-19 14:08:27 +0100

    feat: basic logic to play the game in console

--- main.js
+++ main.js
@@ -1,47 +1,84 @@
 const Player = (name, marker) => {
-    const getName = () => name;
-    const getMarker = () => marker; 
-    return {
-      getName,
-      getMarker
-    };
+    return { name, marker };
 };
 
 const Gameboard = (() => {
-    let board;
-  
-    const createBoard = () => {
-        board = [];
-        for (let i = 0; i < 3; i++){
-            let row = [];
-            for (let j = 0; j < 3; j++){
-                row.push("");
-            }
-            board.push(row);
-        }
-    };
+    let board = ['', '', '', '', '', '', '', '', ''];
   
     const getBoard = () => board;
   
-    const placeMarker = (row, col, marker) => {
-      if (board[row][col] === "") {
-        board[row][col] = marker;
+    const placeMarker = (index, marker) => {
+      if (board[index] === "") {
+        board[index] = marker;
+        console.log(getBoard())
         return true;
       } else {
+        console.log(getBoard())
         return false;
       }
     };
 
     const resetBoard = () => {
-      createBoard();
+      board = ['', '', '', '', '', '', '', '', ''];
     };
-  
-    createBoard();
   
     return {
       getBoard,
       placeMarker,
       resetBoard
     };
-  })();
+})();
+
+const GameState = (() => {
+  let currentPlayer;
   
+  const WINNING_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+    [0, 4, 8], [2, 4, 6]              
+  ];
+
+  const startGame = () => {
+    // Logic to start the game, initialize players, etc.
+    // For simplicity, let's assume we have two players 'X' and 'O'
+    currentPlayer = Player('Player 1', 'X');
+  };
+
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer.marker === 'X' ? Player('Player 2', 'O') : Player('Player 1', 'X');
+  };
+
+  const checkWinner = () => {
+    const board = Gameboard.getBoard();
+    for (let combination of WINNING_COMBINATIONS) {
+      const [a, b, c] = combination;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return currentPlayer; 
+      }
+    }
+
+    if (board.every(cell => cell !== '')) {
+      return 'tie'; 
+    }
+
+    return null; 
+  };
+
+  const playMove = (index) => {
+    if (Gameboard.placeMarker(index, currentPlayer.marker)) {
+      const winner = checkWinner();
+      if (winner) {
+        console.log(winner === 'tie' ? 'It\'s a tie!' : `${winner.name} wins!`);
+      } else {
+        switchPlayer();
+      }
+    } else {
+      console.log('Spot already taken. Choose another spot.');
+    }
+  };
+
+
+
+  return { startGame, playMove };
+})();
+
